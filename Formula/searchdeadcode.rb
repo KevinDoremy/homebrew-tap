45efc8d185864c584e4b854f739463d31102c88c
Kevin Doremy <65405378+KevinDoremy@users.noreply.github.com>
2026-08-01 13:25:25 +0000

    searchdeadcode 0.15.1

--- Formula/searchdeadcode.rb
+++ Formula/searchdeadcode.rb
@@ -6,28 +6,28 @@
 class Searchdeadcode < Formula
   desc "Fast CLI tool to detect dead/unused code in Android projects (Kotlin & Java)"
   homepage "https://github.com/KevinDoremy/SearchDeadCode"
-  version "0.15.0"
+  version "0.15.1"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/KevinDoremy/SearchDeadCode/releases/download/v0.15.0/searchdeadcode-macos-x86_64"
-      sha256 "943a2358e5d3959d97fc26db4fff8fc5cd5be785bc36974eec59c75c68c16e34"
+      url "https://github.com/KevinDoremy/SearchDeadCode/releases/download/v0.15.1/searchdeadcode-macos-x86_64"
+      sha256 "20e733ee9f827488bc440f219510c521075444d0bfe75e37dcd7833421ab2ea9"
     end
     on_arm do
-      url "https://github.com/KevinDoremy/SearchDeadCode/releases/download/v0.15.0/searchdeadcode-macos-aarch64"
-      sha256 "ca47fbe78a486439806a737b3dedd8fd1cf9eddc3095472cc1ee008a70ab6d55"
+      url "https://github.com/KevinDoremy/SearchDeadCode/releases/download/v0.15.1/searchdeadcode-macos-aarch64"
+      sha256 "4ba435be1c6063d0bf015597005d5b93b0dae17505a90860e230ae58889cde4b"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/KevinDoremy/SearchDeadCode/releases/download/v0.15.0/searchdeadcode-linux-x86_64"
-      sha256 "9275687e70004005dbc4dcd0e6dbb2aee30c6b248616d017f6bcc57fe83b57e4"
+      url "https://github.com/KevinDoremy/SearchDeadCode/releases/download/v0.15.1/searchdeadcode-linux-x86_64"
+      sha256 "4a05854d67dc1bfb0e914d50cf0f8d7035325b2a7a55b754d569a10ae5bcc3a6"
     end
     on_arm do
-      url "https://github.com/KevinDoremy/SearchDeadCode/releases/download/v0.15.0/searchdeadcode-linux-aarch64"
-      sha256 "99eda7cd74456cd0b8a0b7078d972843c896a6f8dcad4e8dcab900eb7f0898f7"
+      url "https://github.com/KevinDoremy/SearchDeadCode/releases/download/v0.15.1/searchdeadcode-linux-aarch64"
+      sha256 "95bc1f9843b38ca4ff31b830e4211c8f7842a04b61468364e9b16447468e2f6c"
     end
   end
 
@@ -38,6 +38,6 @@ class Searchdeadcode < Formula
   end
 
   test do
-    assert_match "0.15.0", shell_output("#{bin}/searchdeadcode --version")
+    assert_match "0.15.1", shell_output("#{bin}/searchdeadcode --version")
   end
 end
